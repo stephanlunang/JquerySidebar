ea6a1f21023641f9643f716d4a451c6a47d991b1
Stephan Ng <stephanlunang@gmail.com>
2016-01-27 19:29:32 -0800

    Add yellow highlight on links.

--- sidebar.js
+++ sidebar.js
@@ -5,10 +5,10 @@ if (Meteor.isClient) {
     function ($scope) {
       function DeployMenu (callback){
         $('.menubutton').on('click', function (){
-          $('.leftnav').css({"box-shadow" : "5px 5px 5px #888888"});
+          $('.leftnav').css({"box-shadow" : "2px 2px 2px #888888"});
           $('.leftnav').animate({left : "0px"}, 500, function(){
             $('.main').click(function() {
-              $('.leftnav').animate({left: "-300px"}, 500);
+              $('.leftnav').animate({left: "-302px"}, 500);
               $('.leftnav').css({"box-shadow" : "none"});
             });
             $('.leftnav').click(function(event){
